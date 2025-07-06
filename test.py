1a7a5a8f368e52c98f957c66a56721167476e002
jayasurya88 <jayasurya7879@gmail.com>
2025-07-07 00:04:16 +0530

    fixed

--- test.py
+++ test.py
@@ -3,13 +3,12 @@ from app import app
 
 @pytest.fixture
 def client():
-    with app.TestClient() as client:
+    with app.test_client() as client:
         yield client
 
 
-
-def test _app_is_working(client):
+def test_app_is_working(client):
     response = client.get('/')
     assert response.status_code == 200
-    assert b"Hello,World!" in response.data
+    assert b"Hello World!" in response.data
 
